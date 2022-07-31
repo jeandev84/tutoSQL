45929d3745f4ff35781c7b971769019d92163e87
jeandev84 <jeandev84@yahoo.com>
2022-07-31 12:01:25 +0300

    join

--- tuto.sql
+++ tuto.sql
@@ -4,12 +4,17 @@ PRAGMA foreign_keys = ON;
 
 -- Suppression des tables de liaison ensuite la table liees
 DROP TABLE IF EXISTS categories_recipes;
-DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS ingredients_recipes;
 
 -- Suppression des tables ordinaires
+DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS recipes;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS ingredients;
 
+
+
+-- Creation de table users
 CREATE TABLE users (
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   username VARCHAR(150) NOT NULL UNIQUE,
@@ -17,6 +22,8 @@ CREATE TABLE users (
 );
 
 
+
+-- Creation de table recipes (recette)
 CREATE TABLE recipes (
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   title VARCHAR(150) NOT NULL,
@@ -28,13 +35,14 @@ CREATE TABLE recipes (
 );
 
 
+-- Creation de table categories
 CREATE TABLE categories (
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   title VARCHAR(150) NOT NULL
 );
 
 
--- TABLE DE LIAISON : categories et recipes
+-- Create de table intermediaire ou table de liaison : categories_recipes
 CREATE TABLE categories_recipes (
   recipe_id INTEGER NOT NULL,
   category_id INTEGER NOT NULL,
@@ -47,13 +55,22 @@ CREATE TABLE categories_recipes (
 
 
 -- Creation de table d' ingredients
-CREATE TABLE categories_recipes (
+CREATE TABLE ingredients (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  name VARCHAR(150) NOT NULL
+);
+
+
+-- Creation de table de table de liaison (ingredients_recipes)
+CREATE TABLE ingredients_recipes (
   recipe_id INTEGER NOT NULL,
-  category_id INTEGER NOT NULL,
+  ingredient_id INTEGER NOT NULL,
+  quantity INTEGER,
+  unit VARCHAR(20),
   FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
-  FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
-  PRIMARY KEY (recipe_id, category_id),
-  UNIQUE (recipe_id, category_id)
+  FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE,
+  PRIMARY KEY (recipe_id, ingredient_id),
+  UNIQUE (recipe_id, ingredient_id)
 );
 
 
@@ -68,18 +85,40 @@ INSERT INTO categories (title) VALUES
   ('Dessert'),
   ('Gateau');
 
+
+
 -- Insertion de recipes
 INSERT INTO recipes (title, slug, duration, user_id) VALUES 
 ('Soupe', 'soupe', 10, 1),
 ('Madeleine', 'madeleine', 30, 1);
 
 
--- Insertion d' identifiant a la table intermediaire (categories et de recipes)
+-- Insertion d' identifiant a la table intermediaire (categories_recipes)
 INSERT INTO categories_recipes (recipe_id, category_id) VALUES
 (1, 1),
 (2, 2),
 (2, 3);
 
+
+-- Insertion d' ingredients
+INSERT INTO ingredients (name) VALUES 
+('Sucre'),
+('Farine'),
+('Levure chimique'),
+('Beurre'),
+('Lait'),
+('Oeuf');
+
+
+
+-- Insertion d' identifiant a la table intermediaire (ingredients_recipes)
+INSERT INTO ingredients_recipes (recipe_id, ingredient_id, quantity, unit) VALUES
+(2, 1, 150, 'g'),
+(2, 2, 200, 'g'),
+(2, 3, 8, 'g'),
+(2, 4, 100, 'g'),
+(2, 5, 50, 'g'),
+(2, 6, 3, NULL);
 
 
 -- Selection (JOIN)
@@ -88,12 +127,25 @@ SELECT *
 FROM recipes r
 JOIN categories_recipes cr ON cr.recipe_id = r.id
 JOIN categories c ON cr.category_id = c.id;
-;
-*/
+
 
 SELECT r.title, c.title AS category
 FROM recipes r
 JOIN categories_recipes cr ON cr.recipe_id = r.id
 JOIN categories c ON cr.category_id = c.id;
+;
+
+
+SELECT r.title
+FROM ingredients i
+JOIN ingredients_recipes ir ON ir.ingredient_id = i.id
+JOIN recipes r ON ir.recipe_id = r.id
+WHERE i.name = 'Oeuf';
+*/
+
+
+
+
+
 
 
